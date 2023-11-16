a584fc2e5e31bc15b2ac08caea525261bf38a0b4
Hayden <donnellyhd@outlook.com>
2023-11-15 22:08:10 -0500

    Added save path indication to hlo gen script.

--- scripts/generate_hlo.py
+++ scripts/generate_hlo.py
@@ -7,10 +7,11 @@ def target_fn(x):
 def main():
     hlo_comp = jax.xla_computation(target_fn)(jnp.ones((1,)))
     hlo_comp_text = hlo_comp.as_hlo_text().strip()
-    with open('hlo_modules/hlo_comp.txt', 'w') as f:
+    save_path = 'hlo_modules/hlo_comp.txt'
+    with open(save_path, 'w') as f:
         f.write(hlo_comp_text)
     
-    print('Saved the following HLO computation:\n')
+    print(f'Saved the following HLO computation to {save_path}:\n')
     print(hlo_comp_text)
 
 if __name__ == '__main__':
